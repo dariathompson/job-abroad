37dbb6c541959da0a6b9e64273b32b5281be1ac9
dariathompson <daria.kambalina@gmail.com>
2019-06-21 13:43:33 +0300

    index search

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -24,44 +24,42 @@ let map;
 
 // Initialize and add the map
 function initMap() {
-    // The location of San Sebastian
+    // The location of philadelphia
     let philadelphia = {
         lat: 39.9525839,
         lng: -75.1652215
     };
-    // The map, centered at Uluru
+    // The map, centered at philadelphia
     map = new google.maps.Map(
         document.getElementById('ba-map'), {
-            zoom:8,
+            zoom: 8,
             center: philadelphia
-        });   
-    
-}
+        });
 
+}
 
 $(document).ready(function (e) {
     initMap();
 });
-
-//list of cities
-const endpoint =
-    "data/data.json";
-
-    
-const cities = [];
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data));
+//getting parameter
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
 
 function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
-       
     });
 }
 
+
+//list of cities
+const endpoint = "data/data.json";
+const cities = [];
 
 const searchForm = document.getElementById('search-form');
 const hostResults = document.getElementById('hostResults');
@@ -74,67 +72,92 @@ const searchInput = document.getElementById('search');
 const destination = document.getElementById("ba-destination"); // new
 const baHost = document.getElementById("baHost"); // new
 
+//If we come from index page make search
+
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => {
+        cities.push(...data);
+
+        //get parameter query when we go from index to search page
+        let query = getUrlParameter('query');
+        if (query) {
+            searchInput.value = query;
+            displayMatches();
+        }
+
+    });
+
 function displayMatches(event) {
-    event.preventDefault();
+    if (event) {
+        event.preventDefault();
+    }
+
     const matchArray = findMatches(searchInput.value, cities);
-    const markersCoords = [];
 
+    clearMarkers();
 
-    console.log(map.markers);
-    
+    //check if there is city we search for
+    if (matchArray.length == 0) {
+        baHost.innerHTML = `No hosts`;
+        hostResults.innerHTML = '';
+    } else {
 
-    const html = matchArray.map(place => {
+        const markersCoords = [];
 
-        // The marker, positioned at result
-        markersCoords.push(
-            {
+        const html = matchArray.map(place => {
+
+            // The marker, positioned at result
+            markersCoords.push({
                 lat: place.latitude,
                 lng: place.longitude
-            }
-        );
-        
-        // const regex = new RegExp(searchInput.value, 'gi');
-        let cardHtml  = cardTmpl
-                        .replace(/🦄name🦄/ig, place.personInfo.name)
-                        .replace(/🦄city🦄/ig, place.city)
-                        .replace(/🦄state🦄/ig, place.state)
-                        .replace(/🦄friends🦄/ig, place.personInfo.friends)
-                        .replace(/🦄languages🦄/ig, place.personInfo.languages)
-                        .replace(/🦄references🦄/ig, place.personInfo.references)
-                        .replace(/🦄img🦄/ig, place.personInfo.img)
-                        .replace(/🦄room🦄/ig, place.personInfo.accomodaion)
-                        ;
-                        
-        return cardHtml;
-    }).join('');
-    hostResults.innerHTML = html;
+            });
 
-    // show destination under the map
-    destination.innerHTML = `${matchArray[0].city}, ${matchArray[0].state}`;
+            // const regex = new RegExp(searchInput.value, 'gi');
+            let cardHtml = cardTmpl
+                .replace(/🦄name🦄/ig, place.personInfo.name)
+                .replace(/🦄city🦄/ig, place.city)
+                .replace(/🦄state🦄/ig, place.state)
+                .replace(/🦄friends🦄/ig, place.personInfo.friends)
+                .replace(/🦄languages🦄/ig, place.personInfo.languages)
+                .replace(/🦄references🦄/ig, place.personInfo.references)
+                .replace(/🦄img🦄/ig, place.personInfo.img)
+                .replace(/🦄room🦄/ig, place.personInfo.accomodaion);
 
-    // show number of hosts in location
-    baHost.innerHTML = `${matchArray.length} hosts in ${matchArray[0].city}, ${matchArray[0].state}`;
-    
-    clearMarkers();
-    map.markers = [];
-    // console.log(markersCoords);
-    markersCoords.forEach(coord => {
-        let marker = new google.maps.Marker({
-            icon: 'assets/img/marker.png',
-            position: coord,
-            mapTypeId: 'roadmap',
-            map: map         
+            return cardHtml;
+        }).join('');
+        hostResults.innerHTML = html;
+
+        // show destination under the map
+        destination.innerHTML = `${matchArray[0].city}, ${matchArray[0].state}`;
+
+        // show number of hosts in location
+        baHost.innerHTML = `${matchArray.length} hosts in ${matchArray[0].city}, ${matchArray[0].state}`;
+
+        map.markers = [];
+        // console.log(markersCoords);
+        markersCoords.forEach(coord => {
+            let marker = new google.maps.Marker({
+                icon: 'assets/img/marker.png',
+                position: coord,
+                mapTypeId: 'roadmap',
+                map: map
+            });
+            map.markers.push(marker);
         });
-        map.markers.push(marker);
-    });   
 
-    map.setCenter(markersCoords[0]);
+        map.setCenter(markersCoords[0]);
+
+    }
+
+
 }
-
+//clear markers after search
 function clearMarkers() {
-    if(map.markers) {
+    if (map.markers) {
         for (var i = 0; i < map.markers.length; i++) {
-            map.markers[i].setMap(null);   
+            map.markers[i].setMap(null);
         }
         map.markers = [];
     }
@@ -144,8 +167,8 @@ function clearMarkers() {
 // searchInput.addEventListener('keyup', displayMatches);
 
 // <li>
-    // 	<span class = "name">${place.city}, ${place.state}</span>
-    // 	<span class = "population">${place.population}</span>
+// 	<span class = "name">${place.city}, ${place.state}</span>
+// 	<span class = "population">${place.population}</span>
 // </li>
 
 let today = new Date();
@@ -153,10 +176,10 @@ let today = new Date();
 
 
 $('[data-toggle="datepicker"]').datepicker({
-   format: 'dd.mm.YYYY',
-   weekStart: 1,
-   startView: 0,
-   yearFirst: false,
-   yearSuffix: '',
-   startDate: today
+    format: 'dd.mm.YYYY',
+    weekStart: 1,
+    startView: 0,
+    yearFirst: false,
+    yearSuffix: '',
+    startDate: today
 });
